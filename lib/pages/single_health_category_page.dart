692b0cc666706c3adaaf4c83444f1cad75486590
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-01 08:03:52 +0530

    Add SingleHealthCategoryPage widget with basic structure

---
+++ lib/pages/single_health_category_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SingleHealthCategoryPage extends StatelessWidget {
+  const SingleHealthCategoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
