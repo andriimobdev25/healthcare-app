9192b8849e609e0e6fd896649afe433eaf15153a
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-02 11:25:07 +0530

    Enhance SymptonModel with JSON serialization methods and rename doctorNameImage to doctorNoteImage

--- lib/models/sympton_model.dart
+++ lib/models/sympton_model.dart
@@ -1,15 +1,36 @@
 class SymptonModel {
   final String name;
   final String? medicalReportImage;
-  final String? doctorNameImage;
+  final String? doctorNoteImage;
   final String? precriptionsImage;
   final String? clinicNoteImage;
 
   SymptonModel({
     required this.name,
     this.medicalReportImage,
-    this.doctorNameImage,
+    this.doctorNoteImage,
     this.precriptionsImage,
     this.clinicNoteImage,
   });
+
+  // convert to dart
+  factory SymptonModel.fromJson(Map<String, dynamic> json) {
+    return SymptonModel(
+      name: json['name'] ?? '',
+      medicalReportImage: json['medicalReportImage'] ?? '',
+      doctorNoteImage: json['doctorNoteImage'] ?? '',
+      precriptionsImage: json['precriptionsImage'] ?? '',
+      clinicNoteImage: json['clinicNoteImage'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'medicalReportImage': medicalReportImage,
+      'doctorNoteImage': doctorNoteImage,
+      'precriptionsImage': precriptionsImage,
+      'clinicNoteImage': clinicNoteImage,
+    };
+  }
 }
