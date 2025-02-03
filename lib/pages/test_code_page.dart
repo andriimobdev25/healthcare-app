ecb9bdd96949c0ea6e8c2d242dec1d4d02eb21dd
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-03 07:55:06 +0530

    Implement symptom retrieval and display in TestCodePage with error handling

--- lib/pages/test_code_page.dart
+++ lib/pages/test_code_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:healthcare/models/health_category_model.dart';
+import 'package:healthcare/services/category/symton_service.dart';
 
 class TestCodePage extends StatefulWidget {
   final HealthCategory healthCategory;
@@ -14,6 +18,54 @@ class _TestCodePageState extends State<TestCodePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              StreamBuilder(
+                stream: SymtonService().getSymptoms(
+                  FirebaseAuth.instance.currentUser!.uid,
+                  widget.healthCategory.id,
+                ),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text("Error: ${snapshot.error}"),
+                    );
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Center(
+                      child: Text("No data available"),
+                    );
+                  } else {
+                    final symptons = snapshot.data;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: symptons!.length,
+                      itemBuilder: (context, index) {
+                        final sympton = symptons[index];
+                        return ListTile(
+                          title: Text(sympton.name),
+                          subtitle: Image.memory(
+                            base64Decode(
+                              sympton.medicalReportImage ?? '',
+                            ),
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
