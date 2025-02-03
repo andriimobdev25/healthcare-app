837ab6fc6fb34dd56b3a2a7f09d0abffcb990934
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-03 12:50:02 +0530

    Add methods to retrieve clinics by category and fetch clinic map with category names

--- lib/services/category/clinic_service.dart
+++ lib/services/category/clinic_service.dart
@@ -22,4 +22,50 @@ class ClinicService {
       print("Error add Clinic on service: ${error}");
     }
   }
+
+  // todo: get Clinic with category name
+
+  // get Clinic
+
+  Stream<List<Clinic>> getClinic(String userId, String categoryId) {
+    try {
+      final CollectionReference clinicCollection = userCollection
+          .doc(userId)
+          .collection("healthCategory")
+          .doc(categoryId)
+          .collection("clinc");
+
+      return clinicCollection.snapshots().map((snpshot) {
+        return snpshot.docs
+            .map((doc) => Clinic.fromJson(doc.data() as Map<String, dynamic>))
+            .toList();
+      });
+    } catch (error) {
+      print("error: ${error}");
+      return Stream.empty();
+    }
+  }
+
+  Future<Map<String, List<Clinic>>> getClinicWithCategoryName(
+      String userId) async {
+    try {
+      final healthCategoryCollection =
+          userCollection.doc(userId).collection("healthCategory");
+
+      final QuerySnapshot snapshot = await healthCategoryCollection.get();
+
+      final Map<String, List<Clinic>> clinicMap = {};
+
+      for (final doc in snapshot.docs) {
+        final categoryId = doc.id;
+
+        final List<Clinic> clincs = await getClinic(userId, categoryId).first;
+        clinicMap[doc['name']] = clincs;
+      }
+      return clinicMap;
+    } catch (error) {
+      print("Error fetching clinic map onservice: ${error}");
+      return {};
+    }
+  }
 }
