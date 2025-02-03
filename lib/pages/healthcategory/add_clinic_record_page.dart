7c472bd3b81982e52d8132f7f1e56e0a26a70c9d
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-03 08:42:36 +0530

    Add AddClinicRecordPage with initial placeholder implementation

---
+++ lib/pages/healthcategory/add_clinic_record_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddClinicRecordPage extends StatefulWidget {
+  const AddClinicRecordPage({super.key});
+
+  @override
+  State<AddClinicRecordPage> createState() => _AddClinicRecordPageState();
+}
+
+class _AddClinicRecordPageState extends State<AddClinicRecordPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
