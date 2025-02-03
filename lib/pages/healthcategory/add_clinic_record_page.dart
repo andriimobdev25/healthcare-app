61ce22f9b91cc40d8251f300b5d1b5eba0006c22
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-03 09:00:02 +0530

    Refactor AddClinicRecordPage to use StatelessWidget and initialize date and time values

--- lib/pages/healthcategory/add_clinic_record_page.dart
+++ lib/pages/healthcategory/add_clinic_record_page.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:healthcare/models/health_category_model.dart';
 
-class AddClinicRecordPage extends StatefulWidget {
+class AddClinicRecordPage extends StatelessWidget {
   final HealthCategory healthCategory;
-  const AddClinicRecordPage({
+
+  
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _reasonController = TextEditingController();
+  final TextEditingController _noteController = TextEditingController();
+
+    final ValueNotifier<DateTime> _selectDate =
+      ValueNotifier<DateTime>(DateTime.now());
+  final ValueNotifier<TimeOfDay> _selectTime =
+      ValueNotifier<TimeOfDay>(TimeOfDay.now());
+  
+
+   AddClinicRecordPage({
     super.key,
     required this.healthCategory,
-  });
+  }){
+      _selectDate.value = DateTime.now();
+    _selectTime.value = TimeOfDay.now();
+  }
 
-  @override
-  State<AddClinicRecordPage> createState() => _AddClinicRecordPageState();
-}
 
-class _AddClinicRecordPageState extends State<AddClinicRecordPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+            child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [],
+          ),
+        )),
+      ),
     );
   }
 }
