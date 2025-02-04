9cab0dced24a2f738390b24572664798a68dc5df
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-05 04:39:18 +0530

    Implement data fetching and display logic in LibraryPage with error handling and loading indicator

--- lib/pages/main_screens/library_page.dart
+++ lib/pages/main_screens/library_page.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:healthcare/models/clinic_model.dart';
+import 'package:healthcare/models/health_category_model.dart';
+import 'package:healthcare/models/sympton_model.dart';
 import 'package:healthcare/services/category/clinic_service.dart';
 import 'package:healthcare/services/category/health_category_service.dart';
 import 'package:healthcare/services/category/symton_service.dart';
@@ -18,16 +21,16 @@ class LibraryPage extends StatelessWidget {
           .getClinicWithCategoryName(FirebaseAuth.instance.currentUser!.uid);
 
       return {
-        'healthCategories':healthCategories,
-        'symptons':symptons,
-        'clinics':clinics,
+        'healthCategories': healthCategories,
+        'symptons': symptons,
+        'clinics': clinics,
       };
     } catch (error) {
       print("Error: ${error}");
       return {
-          'healthCategories':{},
-        'symptons':[],
-        'clinics':[],
+        'healthCategories': {},
+        'symptons': [],
+        'clinics': [],
       };
     }
   }
@@ -38,8 +41,52 @@ class LibraryPage extends StatelessWidget {
       appBar: AppBar(),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [],
+          child: FutureBuilder(
+            future: _fetchData(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text("Error: ${snapshot.error}"),
+                );
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          "assets/images/undraw_medical-research_pze7.png",
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text("No health record available yet!"),
+                      ],
+                    ),
+                  ),
+                );
+              } else {
+                final healthCategories =
+                    snapshot.data!['healthCategories'] as List<HealthCategory>;
+                final symptonMap = snapshot.data!['symptons']
+                    as Map<String, List<SymptonModel>>;
+                final clinicMap =
+                    snapshot.data!['clinics'] as Map<String, List<Clinic>>;
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: healthCategories.length,
+                  itemBuilder: (context, index) {
+                    
+                  },
+                );
+              }
+            },
           ),
         ),
       ),
