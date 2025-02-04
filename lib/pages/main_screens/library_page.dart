cee9d6c905520a9ca1f2c95650ba104e55394d25
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-05 04:25:00 +0530

    Add data fetching logic to LibraryPage for health categories, symptoms, and clinics

--- lib/pages/main_screens/library_page.dart
+++ lib/pages/main_screens/library_page.dart
@@ -1,19 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:healthcare/services/category/clinic_service.dart';
+import 'package:healthcare/services/category/health_category_service.dart';
+import 'package:healthcare/services/category/symton_service.dart';
 
 class LibraryPage extends StatelessWidget {
   const LibraryPage({super.key});
 
+  Future<Map<String, dynamic>> _fetchData() async {
+    try {
+      final healthCategories = await HealthCategoryService()
+          .getHealthCategories(FirebaseAuth.instance.currentUser!.uid);
+      final symptons = await SymtonService()
+          .getSymptomsWithCategoryName(FirebaseAuth.instance.currentUser!.uid);
+
+      final clinics = await ClinicService()
+          .getClinicWithCategoryName(FirebaseAuth.instance.currentUser!.uid);
+
+      return {
+        'healthCategories':healthCategories,
+        'symptons':symptons,
+        'clinics':clinics,
+      };
+    } catch (error) {
+      print("Error: ${error}");
+      return {
+          'healthCategories':{},
+        'symptons':[],
+        'clinics':[],
+      };
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Library page"),
-      ),
+      appBar: AppBar(),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
-            children: [
-            ],
+            children: [],
           ),
         ),
       ),
