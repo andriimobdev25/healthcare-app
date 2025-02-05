aa155080eb5e884b506ea2b84ba49d2819cb945e
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-06 04:42:04 +0530

    Refactor health category card layout in LibraryPage for enhanced user experience

--- lib/pages/main_screens/library_page.dart
+++ lib/pages/main_screens/library_page.dart
@@ -42,192 +42,215 @@ class LibraryPage extends StatelessWidget {
       appBar: AppBar(),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: FutureBuilder(
-            future: _fetchData(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text("Error: ${snapshot.error}"),
-                );
-              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-                  child: Center(
-                    child: Column(
-                      children: [
-                        Image.asset(
-                          "assets/images/undraw_medical-research_pze7.png",
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text("No health record available yet!"),
-                      ],
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    labelText: "Search",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        borderSide: Divider.createBorderSide(context)),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: Divider.createBorderSide(context),
                     ),
+                    prefixIcon: const Icon(Icons.search),
                   ),
-                );
-              } else {
-                final healthCategories =
-                    snapshot.data!['healthCategories'] as List<HealthCategory>;
-                final symptonMap = snapshot.data!['symptons']
-                    as Map<String, List<SymptonModel>>;
-                final clinicMap =
-                    snapshot.data!['clinics'] as Map<String, List<Clinic>>;
-                return ListView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: healthCategories.length,
-                  itemBuilder: (context, index) {
-                    final healthCategory = healthCategories[index];
-                    final categorySymptons =
-                        symptonMap[healthCategory.name] ?? [];
-                    final categoryClinics =
-                        clinicMap[healthCategory.name] ?? [];
-                    return Card(
-                      margin: EdgeInsets.only(
-                        bottom: 16,
-                        left: 8,
-                        right: 8,
-                      ),
-                      // color: subLandMarksCardBg,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 20,
-                          horizontal: 12,
-                        ),
+                ),
+              ),
+              FutureBuilder(
+                future: _fetchData(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text("Error: ${snapshot.error}"),
+                    );
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 16),
+                      child: Center(
                         child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Text(
-                              healthCategory.name,
-                              style: TextStyle(
-                                fontSize: 27,
-                                fontWeight: FontWeight.bold,
-                              ),
+                            Image.asset(
+                              "assets/images/undraw_medical-research_pze7.png",
                             ),
                             SizedBox(
-                              height: 10,
+                              height: 20,
                             ),
-                            Text(
-                              healthCategory.description,
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            if (categorySymptons.isNotEmpty) ...[
-                              SizedBox(
-                                height: 16,
-                              ),
-                              Text(
-                                "Symptons",
-                                style: TextStyle(
-                                  fontSize: 21,
-                                  fontWeight: FontWeight.w700,
-                                  color: mainGreenColor,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 16,
-                              ),
-                              Column(
-                                children: categorySymptons.map((sympton) {
-                                  return Container(
-                                    padding: EdgeInsets.symmetric(
-                                      vertical: 10,
-                                    ),
-                                    margin: EdgeInsets.only(bottom: 10),
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                      color: subLandMarksCardBg,
-                                      borderRadius: BorderRadius.circular(8),
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                        vertical: 8,
-                                        horizontal: 16,
-                                      ),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            sympton.name,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.w400,
-                                              fontSize: 17,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  );
-                                }).toList(),
-                              ),
-                            ],
-                            if (categoryClinics.isNotEmpty) ...[
-                              SizedBox(
-                                height: 16,
-                              ),
-                              Text(
-                                "Clinic",
-                                style: TextStyle(
-                                  fontSize: 21,
-                                  fontWeight: FontWeight.w700,
-                                  color: button1,
-                                ),
-                              ),
-                              SizedBox(
-                                height: 16,
-                              ),
-                              Column(
-                                children: categoryClinics.map((clinic) {
-                                  return Container(
-                                    padding: EdgeInsets.symmetric(
-                                      vertical: 10,
-                                    ),
-                                    margin: EdgeInsets.only(bottom: 10),
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                      color: subLandMarksCardBg,
-                                      borderRadius: BorderRadius.circular(8),
-                                    ),
-                                    child: Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                        vertical: 8,
-                                        horizontal: 16,
-                                      ),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            clinic.reason,
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.w400,
-                                              fontSize: 17,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  );
-                                }).toList(),
-                              )
-                            ],
+                            Text("No health record available yet!"),
                           ],
                         ),
                       ),
                     );
-                  },
-                );
-              }
-            },
+                  } else {
+                    final healthCategories = snapshot.data!['healthCategories']
+                        as List<HealthCategory>;
+                    final symptonMap = snapshot.data!['symptons']
+                        as Map<String, List<SymptonModel>>;
+                    final clinicMap =
+                        snapshot.data!['clinics'] as Map<String, List<Clinic>>;
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: healthCategories.length,
+                      itemBuilder: (context, index) {
+                        final healthCategory = healthCategories[index];
+                        final categorySymptons =
+                            symptonMap[healthCategory.name] ?? [];
+                        final categoryClinics =
+                            clinicMap[healthCategory.name] ?? [];
+                        return Card(
+                          margin: EdgeInsets.only(
+                            bottom: 16,
+                            left: 8,
+                            right: 8,
+                          ),
+                          // color: subLandMarksCardBg,
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 20,
+                              horizontal: 12,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  healthCategory.name,
+                                  style: TextStyle(
+                                    fontSize: 27,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  healthCategory.description,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                if (categorySymptons.isNotEmpty) ...[
+                                  SizedBox(
+                                    height: 16,
+                                  ),
+                                  Text(
+                                    "Symptons",
+                                    style: TextStyle(
+                                      fontSize: 21,
+                                      fontWeight: FontWeight.w700,
+                                      color: mainGreenColor,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 16,
+                                  ),
+                                  Column(
+                                    children: categorySymptons.map((sympton) {
+                                      return Container(
+                                        padding: EdgeInsets.symmetric(
+                                          vertical: 10,
+                                        ),
+                                        margin: EdgeInsets.only(bottom: 10),
+                                        width: double.infinity,
+                                        decoration: BoxDecoration(
+                                          color: subLandMarksCardBg,
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            vertical: 8,
+                                            horizontal: 16,
+                                          ),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                sympton.name,
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.w400,
+                                                  fontSize: 17,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                ],
+                                if (categoryClinics.isNotEmpty) ...[
+                                  SizedBox(
+                                    height: 16,
+                                  ),
+                                  Text(
+                                    "Clinic",
+                                    style: TextStyle(
+                                      fontSize: 21,
+                                      fontWeight: FontWeight.w700,
+                                      color: button1,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 16,
+                                  ),
+                                  Column(
+                                    children: categoryClinics.map((clinic) {
+                                      return Container(
+                                        padding: EdgeInsets.symmetric(
+                                          vertical: 10,
+                                        ),
+                                        margin: EdgeInsets.only(bottom: 10),
+                                        width: double.infinity,
+                                        decoration: BoxDecoration(
+                                          color: subLandMarksCardBg,
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                            vertical: 8,
+                                            horizontal: 16,
+                                          ),
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                clinic.reason,
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.w400,
+                                                  fontSize: 17,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                  )
+                                ],
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ],
           ),
         ),
       ),
