ca22aa347a2b0348cc01d73271f683a06ee75309
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-07 17:49:51 +0530

    Add SingleClinicPage widget with basic Scaffold structure

---
+++ lib/pages/healthcategory/single_clinic_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SingleClinicPage extends StatelessWidget {
+  const SingleClinicPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
