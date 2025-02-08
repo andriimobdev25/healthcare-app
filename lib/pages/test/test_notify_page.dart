423c0ef634d5f69fd97358c1b8ba867989a8f626
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-09 01:20:52 +0530

    Add instance notification button to TestNotifyPage

--- lib/pages/test/test_notify_page.dart
+++ lib/pages/test/test_notify_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:healthcare/services/notification/local_notification_service.dart';
 
 class TestNotifyPage extends StatelessWidget {
   const TestNotifyPage({super.key});
@@ -7,6 +8,23 @@ class TestNotifyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  LocalNotificationsService.showInstanceNotification(
+                    title: "instance",
+                    body: "this is instance notification",
+                  );
+                },
+                child: Text("Instace notifications"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
