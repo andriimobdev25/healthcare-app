df9189f805225a9ef6eb9d6783728312305e8ba7
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-09 03:08:33 +0530

    Implement Push Notification Service with Firebase Messaging initialization

---
+++ lib/services/notification/push_notification_service.dart
@@ -0,0 +1,35 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationService {
+
+  static final _firebaseMessaging = FirebaseMessaging.instance;
+
+   //initialize the firebase messaging (request permission for notifications)
+
+  static Future<void> init() async {
+    //request permission for notifications
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      badge: true,
+      provisional: false,
+      sound: true,
+    );
+
+    //check if the permission is granted
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission');
+    } else {
+      print('User declined or has not accepted permission');
+      return;
+    }
+
+    //get the FCM (Firebase Cloud Messagin) token from the device
+    String? token = await _firebaseMessaging.getToken();
+    print('FCM Token: $token');
+  }
+
+
+
+
+
+}
