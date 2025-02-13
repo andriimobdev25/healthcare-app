e5c13a7208fd52c52f0e0e84c78e2f2afa345e3a
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-13 11:48:32 +0530

    Remove unnecessary blank line in add_health_report.dart and add update_sympton_page.dart file

--- lib/pages/healthcategory/add_health_report.dart
+++ lib/pages/healthcategory/add_health_report.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:healthcare/functions/function_dart';
