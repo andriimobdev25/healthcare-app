c11ac795b2e9241b703da90b86254697b03759fb
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-13 11:50:02 +0530

    Add initial implementation of UpdateSymptonPage with basic UI structure

---
+++ lib/pages/healthcategory/update_sympton_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UpdateSymptonPage extends StatefulWidget {
+  const UpdateSymptonPage({super.key});
+
+  @override
+  State<UpdateSymptonPage> createState() => _UpdateSymptonPageState();
+}
+
+class _UpdateSymptonPageState extends State<UpdateSymptonPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
