11cd42671fd39ea1cf8fb8a95a090abe0b916fd3
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-15 04:59:33 +0530

    Remove unnecessary blank line in update symptom page

--- lib/pages/healthcategory/update_sympton_page.dart
+++ lib/pages/healthcategory/update_sympton_page.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:healthcare/functions/function_dart';
 import 'package:healthcare/models/sympton_model.dart';
