c61e9d2c4f986c6d88343e9ac5e7e8150bc50e34
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-21 06:25:23 +0530

    Add flutter_native_timezone import to MedicationReminderService

--- lib/services/notification/meditation_remender_service.dart
+++ lib/services/notification/meditation_remender_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz_data;
 
