1c794fbe5c00667aecdd4c35a395483eaab9cbe0
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-21 09:19:19 +0530

    Remove MedicationReminderService and its integration to streamline notification handling

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:healthcare/firebase_options.dart';
 import 'package:healthcare/provider/theme_provider.dart';
 import 'package:healthcare/router/router.dart';
 import 'package:healthcare/services/notification/local_notification_service.dart';
-import 'package:healthcare/services/notification/meditation_remender_service.dart';
 import 'package:healthcare/services/notification/notification_service.dart';
 import 'package:healthcare/services/notification/push_notification_service.dart';
 import 'package:provider/provider.dart';
@@ -26,8 +25,6 @@ void main() async {
   await PushNotificationsService.init();
 
   await ClinicNotificationService.init();
-
-  await MedicationReminderService.init();
 
   //listen for incoming messages in background
   FirebaseMessaging.onBackgroundMessage(
