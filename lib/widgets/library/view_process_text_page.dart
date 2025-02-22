1a8bd352091d76b768541e9cf5a5c595bbb0371b
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-23 03:33:37 +0530

    Add ViewProcessTextPage widget for displaying processed text content

---
+++ lib/widgets/library/view_process_text_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ViewProcessTextPage extends StatelessWidget {
+  final String title;
+  final String processText;
+  const ViewProcessTextPage({
+    super.key,
+    required this.title,
+    required this.processText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
