fa81150d848d4287ed36f0bce1c9d603a658baf6
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-25 04:59:02 +0530

    No code changes made; skipping commit.

--- lib/widgets/single_category/single_category_image_card.dart
+++ lib/widgets/single_category/single_category_image_card.dart
@@ -30,220 +30,224 @@ class SingleCategoryImageCard extends StatefulWidget {
 class _SingleCategoryImageCardState extends State<SingleCategoryImageCard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        // ignore: deprecated_member_use
-        color: subLandMarksCardBg.withOpacity(0.5),
-        border: Border.all(
-          color: Colors.black54,
+    return Card(
+      elevation: 8,
+      shadowColor: Colors.black,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          // ignore: deprecated_member_use
+          color: subLandMarksCardBg.withOpacity(0.5),
+          // border: Border.all(
+          //   color: Colors.black54,
+          // ),
         ),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(5),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  widget.title,
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: mainLandMarksColor,
+        child: Padding(
+          padding: const EdgeInsets.all(5),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 10,
                   ),
-                ),
-                widget.selectedImage != null
-                    ? Container(
-                        width: 40,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          // ignore: deprecated_member_use
-                          color: subLandMarksCardBg.withOpacity(0.9),
-                        ),
-                        child: IconButton(
-                          onPressed: widget.onPressed,
-                          icon: Icon(
-                            Icons.add_a_photo,
-                            size: 20,
-                          ),
-                        ),
-                      )
-                    : SizedBox(),
-              ],
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Divider(),
-            SizedBox(
-              height: 5,
-            ),
-            Column(
-              children: [
-                widget.selectedImage != null
-                    ? ClipRRect(
-                        borderRadius: BorderRadius.circular(10),
-                        child: Image.file(
-                          File(
-                            widget.selectedImage!.path,
-                          ),
-                          height: 200,
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                      )
-                    : Container(
-                        width: 60,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(100),
-                          // ignore: deprecated_member_use
-                          color: subLandMarksCardBg.withOpacity(0.9),
-                        ),
-                        child: IconButton(
-                          onPressed: widget.onPressed,
-                          icon: Icon(
-                            Icons.add_a_photo,
-                          ),
-                        ),
-                      ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            widget.recognizedText.isNotEmpty
-                ? GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        widget.recognizedText = "";
-                      });
-                    },
-                    child: Container(
-                      width: 200,
-                      height: 35,
-                      margin: EdgeInsets.only(bottom: 5),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: mainWhiteColor,
-                        border: Border.all(
-                          color: mainOrangeColor,
-                          width: 2,
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            "Clear this",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15,
-                              color: mainOrangeColor,
-                            ),
-                          ),
-                          Icon(
-                            Icons.clear,
-                            size: 28,
-                            color: mainOrangeColor,
-                          ),
-                        ],
-                      ),
+                  Text(
+                    widget.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: mainLandMarksColor,
                     ),
-                  )
-                : SizedBox(
-                    height: 10,
                   ),
-            widget.recognizedText.isNotEmpty
-                ? Scrollbar(
-                    child: SingleChildScrollView(
-                      child: Padding(
-                        padding: const EdgeInsets.all(5),
-                        child: Container(
-                          padding: EdgeInsets.all(8),
-                          margin: EdgeInsets.only(bottom: 5),
+                  widget.selectedImage != null
+                      ? Container(
+                          width: 40,
+                          height: 40,
                           decoration: BoxDecoration(
-                            border: Border.all(
-                              color: mainPurpleColor,
+                            borderRadius: BorderRadius.circular(100),
+                            // ignore: deprecated_member_use
+                            color: subLandMarksCardBg.withOpacity(0.9),
+                          ),
+                          child: IconButton(
+                            onPressed: widget.onPressed,
+                            icon: Icon(
+                              Icons.add_a_photo,
+                              size: 20,
                             ),
-                            borderRadius: BorderRadius.circular(8),
-                            color: mainWhiteColor,
                           ),
-                          child: SelectableText(
-                            cursorColor: mainOrangeColor,
-                            widget.recognizedText.isEmpty
-                                ? "No text process"
-                                : widget.recognizedText,
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 15),
-                          ),
-                        ),
-                      ),
-                    ),
-                  )
-                : SizedBox(),
-            SizedBox(
-              height: 1,
-            ),
-            if (widget.selectedImage != null) ...[
+                        )
+                      : SizedBox(),
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Divider(),
+              SizedBox(
+                height: 5,
+              ),
               Column(
                 children: [
-                  GestureDetector(
-                    onTap: widget.processImage,
-                    child: Container(
-                      width: 200,
-                      height: 40,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8),
-                        color: mainWhiteColor,
-                        border: Border.all(
-                          color: mainNightLifeColor,
-                          width: 2,
-                        ),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(
-                            "Process Image",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 16,
-                              color: mainPurpleColor,
+                  widget.selectedImage != null
+                      ? ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            File(
+                              widget.selectedImage!.path,
+                            ),
+                            height: 200,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                      : Container(
+                          width: 60,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            // ignore: deprecated_member_use
+                            color: subLandMarksCardBg.withOpacity(0.9),
+                          ),
+                          child: IconButton(
+                            onPressed: widget.onPressed,
+                            icon: Icon(
+                              Icons.add_a_photo,
                             ),
                           ),
-                          widget.isRecognized
-                              ? const SizedBox(
-                                  width: 16,
-                                  height: 16,
-                                  child: CircularProgressIndicator(
-                                    strokeWidth: 2,
-                                    color: Colors.orange,
+                        ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              widget.recognizedText.isNotEmpty
+                  ? GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          widget.recognizedText = "";
+                        });
+                      },
+                      child: Container(
+                        width: 200,
+                        height: 35,
+                        margin: EdgeInsets.only(bottom: 5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: mainWhiteColor,
+                          border: Border.all(
+                            color: mainOrangeColor,
+                            width: 2,
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "Clear this",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                                color: mainOrangeColor,
+                              ),
+                            ),
+                            Icon(
+                              Icons.clear,
+                              size: 28,
+                              color: mainOrangeColor,
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  : SizedBox(
+                      height: 10,
+                    ),
+              widget.recognizedText.isNotEmpty
+                  ? Scrollbar(
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            margin: EdgeInsets.only(bottom: 5),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: mainPurpleColor,
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                              color: mainWhiteColor,
+                            ),
+                            child: SelectableText(
+                              cursorColor: mainOrangeColor,
+                              widget.recognizedText.isEmpty
+                                  ? "No text process"
+                                  : widget.recognizedText,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  : SizedBox(),
+              SizedBox(
+                height: 1,
+              ),
+              if (widget.selectedImage != null) ...[
+                Column(
+                  children: [
+                    GestureDetector(
+                      onTap: widget.processImage,
+                      child: Container(
+                        width: 200,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: mainWhiteColor,
+                          border: Border.all(
+                            color: mainNightLifeColor,
+                            width: 2,
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "Process Image",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: mainPurpleColor,
+                              ),
+                            ),
+                            widget.isRecognized
+                                ? const SizedBox(
+                                    width: 16,
+                                    height: 16,
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 2,
+                                      color: Colors.orange,
+                                    ),
+                                  )
+                                : Icon(
+                                    Icons.auto_awesome,
+                                    size: 30,
+                                    color: mainPurpleColor,
                                   ),
-                                )
-                              : Icon(
-                                  Icons.auto_awesome,
-                                  size: 30,
-                                  color: mainPurpleColor,
-                                ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              )
+                  ],
+                )
+              ],
+              SizedBox(
+                height: 10,
+              ),
             ],
-            SizedBox(
-              height: 10,
-            ),
-          ],
+          ),
         ),
       ),
     );
