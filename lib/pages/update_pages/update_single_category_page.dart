e0cbc5f1463641cd9f2dcb846ff376e03f1e6f9c
andriimobdev25 <andriivynokurov0@gmail.com>
2025-02-28 09:30:26 +0530

    Add initial implementation of UpdateSingleCategoryPage

---
+++ lib/pages/update_pages/update_single_category_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UpdateSingleCategoryPage extends StatefulWidget {
+  const UpdateSingleCategoryPage({super.key});
+
+  @override
+  State<UpdateSingleCategoryPage> createState() => _UpdateSingleCategoryPageState();
+}
+
+class _UpdateSingleCategoryPageState extends State<UpdateSingleCategoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
