962c4f5e88af0d699320f16514490a3c0548d031
andriimobdev25 <andriivynokurov0@gmail.com>
2025-03-03 08:02:20 +0530

    Add AlertContainer widget for displaying alerts in analytics

---
+++ lib/widgets/analytics/alert_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AlertContainer extends StatelessWidget {
+  const AlertContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
