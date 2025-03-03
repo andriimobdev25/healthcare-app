7ec3f2f690c2fd683b122c065a313130d10ea819
andriimobdev25 <andriivynokurov0@gmail.com>
2025-03-03 07:35:50 +0530

    Implement CreateAnalyticCategory widget with card layout and image for analytics feature

--- lib/widgets/analytics/create_analytic_category.dart
+++ lib/widgets/analytics/create_analytic_category.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:healthcare/constants/colors.dart';
 
 class CreateAnalyticCategory extends StatelessWidget {
   const CreateAnalyticCategory({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      elevation: 8,
+      shadowColor: mobileBackgroundColor,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 8,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ClipRect(
+              child: Image.asset(
+                "assets/images/menu_15604342.png",
+                width: 50,
+                height: 50,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text(
+              "Create analytic category",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
