ae137df531f892b48c71aeba9110388d3351149d
andriimobdev25 <andriivynokurov0@gmail.com>
2025-03-03 07:27:45 +0530

    Add CreateAnalyticCategory widget for analytics feature

---
+++ lib/widgets/analytics/create_analytic_category.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateAnalyticCategory extends StatelessWidget {
+  const CreateAnalyticCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
