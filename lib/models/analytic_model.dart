8e9df69dfb16335be1e18c76a678254bb06be411
andriimobdev25 <andriivynokurov0@gmail.com>
2025-03-03 08:37:43 +0530

    Add JSON serialization methods to AnalyticModel class

--- lib/models/analytic_model.dart
+++ lib/models/analytic_model.dart
@@ -6,4 +6,18 @@ class AnalyticModel {
     required this.id,
     required this.name,
   });
+
+  factory AnalyticModel.fromJson(Map<String, dynamic> json) {
+    return AnalyticModel(
+      id: json['id'] ?? '',
+      name: json['name'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+    };
+  }
 }
