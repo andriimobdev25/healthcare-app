505c03f9b009ead8e011a8db06195f9813f5c70a
andriimobdev25 <andriivynokurov0@gmail.com>
2025-03-04 10:57:39 +0530

    Implement StreamBuilder in SignleAnylisticCategoryCard to fetch and display analytic categories

--- lib/widgets/analytics/signle_anylistic_category_card.dart
+++ lib/widgets/analytics/signle_anylistic_category_card.dart
@@ -1,10 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:healthcare/services/analytic/analytic_category_service.dart';
 
 class SignleAnylisticCategoryCard extends StatelessWidget {
   const SignleAnylisticCategoryCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return StreamBuilder(
+      stream: AnalyticCategoryService()
+          .getAnalyticCategory(FirebaseAuth.instance.currentUser!.uid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (snapshot.hasError) {
+          return Center(
+            child: Text("Error: ${snapshot.error}"),
+          );
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(
+            child: Text("Not create category yet"),
+          );
+        } else {
+          return GridView.builder(
+            gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+            ),
+            itemBuilder: (context, index) {
+              
+            },
+          );
+        }
+      },
+    );
   }
 }
