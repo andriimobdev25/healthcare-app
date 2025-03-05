686a74ad784ecc36b9c293b2968eacfc8eec71b4
andriimobdev25 <andriivynokurov0@gmail.com>
2025-03-06 04:55:22 +0530

    Add AddAnalyticDataPage for analytics data entry interface

---
+++ lib/pages/analytics/add_analytic_data_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddAnalyticDataPage extends StatefulWidget {
+  const AddAnalyticDataPage({super.key});
+
+  @override
+  State<AddAnalyticDataPage> createState() => _AddAnalyticDataPageState();
+}
+
+class _AddAnalyticDataPageState extends State<AddAnalyticDataPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
