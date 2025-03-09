c4f13f60f15f5a0b3699937d66b838ef1975271c
andriimobdev25 <andriivynokurov0@gmail.com>
2025-03-09 09:00:08 +0530

    Add FutureBuilder to ShowAnalyticDataPage for async data fetching and error handling

--- lib/pages/analytics/show_analytic_data_page.dart
+++ lib/pages/analytics/show_analytic_data_page.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:healthcare/models/analytic_model.dart';
+import 'package:healthcare/models/blood_suger_data_model.dart';
 import 'package:healthcare/services/analytic/analytic_category_service.dart';
 
 class ShowAnalyticDataPage extends StatefulWidget {
@@ -19,13 +21,13 @@ class _ShowAnalyticDataPageState extends State<ShowAnalyticDataPage> {
               FirebaseAuth.instance.currentUser!.uid);
 
       return {
-        'categories':categories,
-        'analyticData':analyticData,
+        'categories': categories,
+        'analyticData': analyticData,
       };
     } catch (error) {
       return {
-         'categories':[],
-        'analyticData':[],
+        'categories': [],
+        'analyticData': [],
       };
     }
   }
@@ -34,6 +36,42 @@ class _ShowAnalyticDataPageState extends State<ShowAnalyticDataPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: FutureBuilder(
+            future: _fetchData(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text("Error: ${snapshot.error}"),
+                );
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return Center(
+                  child: Text("No AnalyticData available"),
+                );
+              } else {
+                final categories =
+                    snapshot.data!['categories'] as List<AnalyticModel>;
+                final analyticDataMap = snapshot.data!['analyticData']
+                    as Map<String, List<BloodSugerDataModel>>;
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: categories.length,
+                  itemBuilder: (context, index) {
+                    final category = categories[index];
+                    final categoryAnalytic = analyticDataMap[category];
+                  },
+                );
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
 }
