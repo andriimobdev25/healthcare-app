b1d1d2156e5e3a72e48de86d0bf3f95bfc008e90
andriimobdev25 <andriivynokurov0@gmail.com>
2025-03-09 08:49:49 +0530

    Add data fetching logic to ShowAnalyticDataPage for categories and analytics data

--- lib/pages/analytics/show_analytic_data_page.dart
+++ lib/pages/analytics/show_analytic_data_page.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:healthcare/services/analytic/analytic_category_service.dart';
 
 class ShowAnalyticDataPage extends StatefulWidget {
   const ShowAnalyticDataPage({super.key});
@@ -8,6 +10,26 @@ class ShowAnalyticDataPage extends StatefulWidget {
 }
 
 class _ShowAnalyticDataPageState extends State<ShowAnalyticDataPage> {
+  Future<Map<String, dynamic>> _fetchData() async {
+    try {
+      final categories = await AnalyticCategoryService()
+          .getAnalyticCategoryAsFuture(FirebaseAuth.instance.currentUser!.uid);
+      final analyticData = await AnalyticCategoryService()
+          .getAnalyticDataByCategoryName(
+              FirebaseAuth.instance.currentUser!.uid);
+
+      return {
+        'categories':categories,
+        'analyticData':analyticData,
+      };
+    } catch (error) {
+      return {
+         'categories':[],
+        'analyticData':[],
+      };
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
